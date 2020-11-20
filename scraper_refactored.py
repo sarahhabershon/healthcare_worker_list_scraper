41c0094b99d8d04a04f5cdfe947a47116dc193f1
Sarah Habershon <habershon@gmail.com>
2020-11-20 13:56:32 +0100

    added NNU lists

--- scraper_refactored.py
+++ scraper_refactored.py
@@ -105,8 +105,16 @@ indonesia = pd.DataFrame(indo, columns=["name"])
 indonesia["country"] = "Indonesia"
 indonesia["source"] = "Indonesia list"
 
+#load NNU lists
+nnu_hcw = pd.read_csv("NNU_HCW.csv", encoding = "mac_roman", names = ["name", "country", "source"])
+print(nnu_hcw)
+
+nnu_nurses = pd.read_csv("nurses_clean.csv", encoding = "mac_roman", names = ["name", "age", "country", "source"])
+print(nnu_nurses)
 #attach all the dfs together
-total = in_memoriam.append(italian_doctors).append(outside_russia).append(russia).append(indonesia)
+total = in_memoriam.append(italian_doctors).append(outside_russia).append(russia).append(indonesia).append(nnu_hcw).append(nnu_nurses)
+
+
 
 #create extra column for sorting out countries
 total["country_or_us_state"] = total["country"]
@@ -130,22 +138,27 @@ total.loc[total['country_or_us_state'].str.contains("LPR"),'country'] = 'Ukraine
 total.loc[total['country_or_us_state'].str.contains("Lviv"),'country'] = 'Ukraine' #sorry :-/
 
 
-#identify US states
-states = [ "Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming", "DC", "Brooklyn"]
+#identify US
+murica = [ "Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming", "DC", "District of Columbia", "Brooklyn", "Puerto Rico", "Guam", "New York City", "Virgin Islands"]
 
-total["USA"] = total["country"].isin(states)
+total["USA"] = total["country_or_us_state"].isin(murica)
 
 total.loc[total.USA == True, "country"] = "USA"
+
+total.to_csv("rawoutputwithduplicates.csv")
 
 #check for duplicates, remove, keep as separate file for checking
 duplicates = total.loc[total[['name', 'age', 'country']].duplicated(), :]
 total = total.drop_duplicates(subset = ['name', 'age', 'country'])
+
+
 
 #count
 grouped = total.groupby(['country'])['name'].agg('count')
 
 
 #create csv output
+
 duplicates.to_csv('duplicates.csv')
 grouped.to_csv("country_count.csv")
 total.to_csv("merged_crowdsourced_lists.csv")
